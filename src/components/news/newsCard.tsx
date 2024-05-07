94ca877000b38ccf4ed7f148206f2e02c75eca2f
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2024-05-07 15:57:13 +0300

    mu

--- src/components/news/newsCard.tsx
+++ src/components/news/newsCard.tsx
@@ -3,43 +3,44 @@ import { CiUser } from "react-icons/ci";
 import { FaRegComments } from "react-icons/fa";
 
 type NewsCardProps = {
-    imageUrl: string;
-    title: string;
-    date: string;
-    author: string;
-    comments?: string[];
-}
+  imageUrl: string;
+  title: string;
+  date: string;
+  author: string;
+  comments?: string[];
+};
 
-const NewsCard:React.FC<NewsCardProps> = ({imageUrl, title, date, author, comments}) => {
-    return (
-        <div className="flex flex-col w-76 py-10 justify-center items-center gap-6 shadow-xl mt-10 rounded-lg">
-            <img className="w-full h-60 rounded-lg" src={imageUrl} alt="news photo" />
-            <div className="w-4/5 mx-auto flex flex-col items-start justify-center gap-4">
-                <div className="w-full flex flex-nowrap justify-start items-center gap-2">
-                    <FaCalendarAlt size={10} color="black" />
-                    <p>{date}</p>
-                </div>
-                <p className="text-xl font-bold text-gray-900 text-start ">
-                    {title}
-                </p>
-                <hr className="w-full text-gray-600"/>
-                <div className="w-full py-6 flex justify-between items-center ">
-                    <div className="flex justify-start flex-nowrap items-center gap-2">
-                        <CiUser size={20} color="black" />
-                        <p>{author}</p>
-                    </div>
-                    <p className="text-xl font-bold text-gray-900"> 
-                        -
-                    </p>
-                    <div className="flex justify-start flex-nowrap items-center gap-2">
-                        <FaRegComments size={20} color="black" />
-                        <p>comments</p>
-                    </div>
-                </div>
-            </div>
-
+const NewsCard: React.FC<NewsCardProps> = ({
+  imageUrl,
+  title,
+  date,
+  author,
+  comments,
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-6 py-10 mt-10 rounded-lg shadow-xl w-76">
+      <img className="w-full rounded-lg h-60" src={imageUrl} alt="news photo" />
+      <div className="flex flex-col items-start justify-center w-4/5 gap-4 mx-auto">
+        <div className="flex items-center justify-start w-full gap-2 flex-nowrap">
+          <FaCalendarAlt size={10} color="black" />
+          <p>{date}</p>
         </div>
-    )
-}
+        <p className="text-xl font-bold text-gray-900 text-start ">{title}</p>
+        <hr className="w-full text-gray-600" />
+        <div className="flex items-center justify-between w-full py-6 ">
+          <div className="flex items-center justify-start gap-2 flex-nowrap">
+            <CiUser size={20} color="black" />
+            <p>{author}</p>
+          </div>
+          <p className="text-xl font-bold text-gray-900">-</p>
+          <div className="flex items-center justify-start gap-2 flex-nowrap">
+            <FaRegComments size={20} color="black" />
+            <p>{comments?.length} Comments</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default NewsCard;
